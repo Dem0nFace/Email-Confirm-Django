796120d27d23121a001975c09e7c21c037a02f8c
Assem Allam <91134613+Dem0nFace@users.noreply.github.com>
2024-03-04 03:09:41 +0800

    Update settings.py

--- EmailConfirmLoginDjango/settings.py
+++ EmailConfirmLoginDjango/settings.py
@@ -108,8 +108,8 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = "index"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_HOST = " itiad@istu.edu"
+EMAIL_PORT = 25
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER="your email"
