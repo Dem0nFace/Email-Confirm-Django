a576c92d30259b5e84cbf7decc8fdbcbbcf59e09
Assem Allam <91134613+Dem0nFace@users.noreply.github.com>
2024-03-03 17:49:23 +0800

    Update forms.py

--- user/forms.py
+++ user/forms.py
@@ -4,7 +4,6 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    phone_no = forms.CharField(max_length = 20)
     first_name = forms.CharField(max_length = 20)
     last_name = forms.CharField(max_length = 20)
     class Meta:  #used to provide metadata about the form
